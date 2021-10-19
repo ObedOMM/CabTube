800f3517619b24b5a7d85084ba4614afeae41955
Obed Mulumba <ommartiste007@gmail.com>
2021-10-19 23:28:55 +0200

    created script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+var menuIcon = document.querySelector(".menu-icon");
+var menuIsidebarcon = document.querySelector(".sidebar");
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+}
